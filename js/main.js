554a8aefd3e7c42c70f8f0d862aa6489c337a249
Tal Maoz <tal.maoz.work@gmail.com>
2019-06-03 13:19:14 +0300

    elipsie multiline working (javascript solution) :)

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 'use strict'
 
+function onInit() {
+    var elsPostParagraphs = document.querySelectorAll('.post-paragraph')
+    for (var elPostParagraph of elsPostParagraphs) {
+        console.log(elPostParagraph)
+        ellipsizeTextBox(elPostParagraph)
+    }
+}
+
+function ellipsizeTextBox(elPostParagraph) {
+    var words = elPostParagraph.innerText.split(' ');
+    while(elPostParagraph.scrollHeight > elPostParagraph.offsetHeight) {
+        words.pop();
+        elPostParagraph.innerText = words.join(' ') + '...';
+    }
+}
