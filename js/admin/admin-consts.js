ee86d28974dbe6580fb2e0e35cd9851001986a5a
Tal Maoz <tal.maoz.work@gmail.com>
2019-06-03 07:54:30 +0300

    design to live page starting to look good :)

--- js/admin/admin-consts.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const KEY_USER_NAME    = 'userName'
-const KEY_PASSWORD     = 'password'
-const KEY_REGISTRATION = 'registrationDate'
-const KEY_ADMIN        = 'admin'
-const KEY_LAST_LOGIN   = 'lastLogin'
--- js/login/login-view.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-function msgUser(msg) {
-    document.querySelector(".msg-login-or-register").innerText = msg
-}
